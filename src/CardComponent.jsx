56dd6d035c501e76ce0ea209dd0d915dda19242f
phemi93 <phemysteven@gmail.com>
2024-12-31 12:22:28 +0100

    card component with API integration

--- src/CardComponent.jsx
+++ src/CardComponent.jsx
@@ -2,45 +2,60 @@ import { useEffect, useState } from "react";
 
 export default function CardComponent() {
   const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(function () {
-    fetch("https://reqres.in/api/users").then((res) =>
-      res.json().then((res) => setDetails(res.data))
-    );
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setDetails(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
-      {details.length > 0 &&
-        details.map((details, index) => (
-          <div
-            key={index}
-            className="w-[30rem] h-full flex flex-row  gap-8 p-6 justify-start items-center border"
-          >
-            <div className="w-[10rem] h-full">
-              <img
-                className="w-full h-full"
-                src={details.avatar}
-                alt={details.first_name}
-              />
-            </div>
+      {details.map((user) => (
+        <div
+          key={user.id}
+          className="w-full sm:w-[30rem] h-full flex flex-col sm:flex-row gap-8 p-6 justify-start items-center border"
+        >
+          <div className="w-[10rem] h-full">
+            <img
+              className="w-full h-full"
+              src={user.avatar}
+              alt={user.first_name}
+              loading="lazy"
+            />
+          </div>
 
-            <div className="w-1/2 h-auto flex flex-col gap-6 justify-start items-start ">
-              <div className="flex flex-row gap-1">
-                <p className="text-md font-medium text-black/60">
-                  {details.first_name}
-                </p>
-                <p className="text-md font-medium text-black/60">
-                  {details.last_name}
-                </p>
-              </div>
-
+          <div className="w-full sm:w-1/2 h-auto flex flex-col gap-6 justify-start items-start">
+            <div className="flex flex-row gap-1">
               <p className="text-md font-medium text-black/60">
-                {details.email}
+                {user.first_name}
+              </p>
+              <p className="text-md font-medium text-black/60">
+                {user.last_name}
               </p>
             </div>
+
+            <p className="text-md font-medium text-black/60">{user.email}</p>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
 }
